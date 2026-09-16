041ece12419e080d25eaec82336dfc2d6ebd1eae
michaelochieng0 <mclay@kabarak.ac.ke>
2026-09-16 15:05:34 +0300

    fix: renaming http.Response coz i had httpresponse earlier

--- api/helpers.go
+++ api/helpers.go
@@ -6,7 +6,7 @@ import (
 )
 
 //this function writes a standardized JSON response
-func JSONResponse (w httpResponseWriter, status int, data any){
+func JSONResponse (w http.ResponseWriter, status int, data any){
 	w.header().Set("Content-Type", "application/json")
 	w.Writeheader(status)
 	if data !=nil{
@@ -15,6 +15,6 @@ func JSONResponse (w httpResponseWriter, status int, data any){
 }
 
 //this other function writes a standardized error message(json)
-func ErrorResponse(w httpResponseWriter, status int, data any){
+func ErrorResponse(w http.ResponseWriter, status int, data any){
 	JSONResponse(w, status, map[string]string{"error": message})
 }
